6cb6130a5645e516a5ab815600af75746c41f08b
Erick Alvarez <erick505alvarez@gmail.com>
2025-04-21 18:13:46 -0500

    Remove fontawesome icons. Add svg checkmark.

--- src/assets/icons.ts
+++
@@ -1,6 +0,0 @@
-// src/assets/icons.ts
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
-
-// Add the icon to the Font Awesome library
-library.add(faCheck);
